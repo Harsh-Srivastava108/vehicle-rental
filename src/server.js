0f3511e6a881b0f4ddc8c76d165dc5ac1f464b76
Harsh Srivastava <harshsrivastava06425@gmail.com>
2025-08-22 02:35:18 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -18,7 +18,12 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(morgan('dev'));
 
